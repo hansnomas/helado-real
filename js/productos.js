335c12e2d234003cb77dfc0895d4572f3ed2449b
hansnomas <118777531+hansnomas@users.noreply.github.com>
2022-12-29 12:43:21 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const productos = [];
+
+productos[0] = [];
+productos[0]['Nombre'] = 'UCC Milk Coffee Rei';
+productos[0]['Precio'] = 400;
+productos[0]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[0]['Imagen'] = 'ucc-rei.jpg';
+
+productos[1] = [];
+productos[1]['Nombre'] = 'UCC Milk Coffee Shinji';
+productos[1]['Precio'] = 400;
+productos[1]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[1]['Imagen'] = 'ucc-shinji.jpg';
+
+productos[2] = [];
+productos[2]['Nombre'] = 'UCC Milk Coffee Asuka';
+productos[2]['Precio'] = 400;
+productos[2]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[2]['Imagen'] = 'ucc-asuka.jpg';
+
+productos[3] = [];
+productos[3]['Nombre'] = 'UCC Milk Coffee Kaworu';
+productos[3]['Precio'] = 400;
+productos[3]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[3]['Imagen'] = 'ucc-kaworu.jpg';
+
+productos[4] = [];
+productos[4]['Nombre'] = 'UCC Milk Coffee Mari';
+productos[4]['Precio'] = 400;
+productos[4]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[4]['Imagen'] = 'ucc-mari.jpg';
+
+productos[5] = [];
+productos[5]['Nombre'] = 'UCC Milk Coffee Misato';
+productos[5]['Precio'] = 400;
+productos[5]['Descripcion'] = 'Diseño EXCLUSIVO de Evangelion 2.0';
+productos[5]['Imagen'] = 'ucc-misato.jpg';
+
+productos[6] = [];
+productos[6]['Nombre'] = 'UCC Milk Canned Coffee';
+productos[6]['Precio'] = 350;
+productos[6]['Descripcion'] = 'Un café con leche clásico sin edulcorantes ni saborizantes artificiales.';
+productos[6]['Imagen'] = 'ucc-mc.jpg';
+
+productos[7] = [];
+productos[7]['Nombre'] = 'UCC Milk Coffee Pack';
+productos[7]['Precio'] = 500;
+productos[7]['Descripcion'] = 'Café con leche 100% descafeinado sin edulcorantes ni saborizantes artificiales, apto incluso para niños.';
+productos[7]['Imagen'] = 'ucc-mcp.jpg';
+
+productos[8] = [];
+productos[8]['Nombre'] = 'UCC Gold Special Special Blend Roasted Coffee';
+productos[8]['Precio'] = 1000;
+productos[8]['Descripcion'] = 'Mezcla brillante de un tueste lleno de sabor y un cuerpo medio.';
+productos[8]['Imagen'] = 'ucc-gssb.jpg';
